ae0da76cde8e39325f40b4f0bd5f90b1cf8821a4
Orçun Aşık <asikorcun@gmail.com>
2024-12-19 16:51:52 +0300

    Created IRepositoryDal base interface for CRUD

---
+++ CoreFoody.DataAccessLayer/Abstract/IRepositoryDal.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreFoody.DataAccessLayer.Abstract;
+public interface IRepositoryDal<T> where T : class, new()
+{
+    void Insert(T entity);
+    void Update(T entity);
+    void Delete(int id);
+    List<T> GetAll();
+    T GetById(int id);
+}
