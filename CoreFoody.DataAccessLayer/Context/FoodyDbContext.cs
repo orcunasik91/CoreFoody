be8abe4f681c6a5ffcab2bea456a1130fec726ce
Orçun Aşık <asikorcun@gmail.com>
2024-12-19 17:38:44 +0300

    Created DbContext classs and declared models

---
+++ CoreFoody.DataAccessLayer/Context/FoodyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CoreFoody.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreFoody.DataAccessLayer.Context;
+public class FoodyDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=CoreFoodyDb;User ID=sa;Password=Password1;");
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<About> Abouts { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Feature> Features { get; set; }
+    public DbSet<SocialMedia> SocialMedias { get; set; }
+    public DbSet<Slider> Sliders { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+}
