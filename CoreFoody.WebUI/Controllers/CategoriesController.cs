8f27d548ef2a444892f8ee6115940250f5df07c4
Orçun Aşık <asikorcun@gmail.com>
2024-12-19 21:14:56 +0300

    Created Category Controller and CategoryList view

---
+++ CoreFoody.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using CoreFoody.BusinessLayer.Abstract;
+using CoreFoody.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFoody.WebUI.Controllers;
+public class CategoriesController : Controller
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoriesController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    public IActionResult CategoryList()
+    {
+        List<Category> categories = _categoryService.GetAll();
+        return View(categories);
+    }
+}
