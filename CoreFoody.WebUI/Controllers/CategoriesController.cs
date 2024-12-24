36842b5b479312fbe874964e78d080aed5070816
Orçun Aşık <asikorcun@gmail.com>
2024-12-24 14:00:08 +0300

    Create, Update and Remove actions and views pages.

--- CoreFoody.WebUI/Controllers/CategoriesController.cs
+++ CoreFoody.WebUI/Controllers/CategoriesController.cs
@@ -11,10 +11,38 @@ public class CategoriesController : Controller
     {
         _categoryService = categoryService;
     }
-
+    [HttpGet]
     public IActionResult CategoryList()
     {
         List<Category> categories = _categoryService.GetAll();
         return View(categories);
+    }
+    [HttpGet]
+    public IActionResult CreateCategory()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult CreateCategory(Category category)
+    {
+        _categoryService.Insert(category);
+        return RedirectToAction(nameof(CategoryList));
+    }
+    public IActionResult RemoveCategory(int id)
+    {
+        _categoryService.Delete(id);
+        return RedirectToAction(nameof(CategoryList));
+    }
+    [HttpGet]
+    public IActionResult UpdateCategory(int id)
+    {
+        Category category = _categoryService.GetById(id);
+        return View(category);
+    }
+    [HttpPost]
+    public IActionResult UpdateCategory(Category category)
+    {
+        _categoryService.Update(category);
+        return RedirectToAction(nameof(CategoryList));
     }
 }
