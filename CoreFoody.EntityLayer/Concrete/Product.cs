dfc39dc2360e221399b6be3e4c4ce8f04d1cc0a6
Orçun Aşık <asikorcun@gmail.com>
2024-12-24 14:11:01 +0300

    Created ProductCategory Relationship and updated database with migration

--- CoreFoody.EntityLayer/Concrete/Product.cs
+++ CoreFoody.EntityLayer/Concrete/Product.cs
@@ -7,4 +7,6 @@ public class Product
     public decimal Price { get; set; }
     public string ImageUrl { get; set; }
     public string Description { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
 }
