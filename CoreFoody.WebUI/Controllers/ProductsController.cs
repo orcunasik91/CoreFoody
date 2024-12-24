bd6fd684007543bd9365e6fae2d766e597edcf88
Orçun Aşık <asikorcun@gmail.com>
2024-12-24 14:56:19 +0300

    Created Products controller and List views

---
+++ CoreFoody.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using CoreFoody.BusinessLayer.Abstract;
+using CoreFoody.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFoody.WebUI.Controllers;
+public class ProductsController : Controller
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+    [HttpGet]
+    public IActionResult ProductList()
+    {
+        List<Product> products = _productService.GetAll();
+        return View(products);
+    }
+    [HttpGet]
+    public IActionResult ProductListWithCategory()
+    {
+        List<Product> productsWithCategory = _productService.ProductsWithCategory();
+        return View(productsWithCategory);
+    }
+}
