2f69d7cb454547a4425fe4703b400ab77b849c57
Orçun Aşık <asikorcun@gmail.com>
2024-12-25 23:54:27 +0300

    Mapped with dto objects for About and Slider models

---
+++ CoreFoody.WebUI/Mappings/GeneralMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CoreFoody.DtoLayer.Dtos.AboutDtos;
+using CoreFoody.DtoLayer.Dtos.SliderDtos;
+using CoreFoody.EntityLayer.Concrete;
+
+namespace CoreFoody.WebUI.Mappings;
+public class GeneralMapping : Profile
+{
+    public GeneralMapping()
+    {
+        #region AboutMapping
+        CreateMap<ResultAboutDto, About>().ReverseMap();
+        CreateMap<CreateAboutDto, About>().ReverseMap();
+        CreateMap<UpdateAboutDto, About>().ReverseMap();
+        CreateMap<GetAboutDto, About>().ReverseMap();
+        #endregion
+        #region SliderMapping
+        CreateMap<ResultSliderDto, Slider>().ReverseMap();
+        CreateMap<CreateSliderDto, Slider>().ReverseMap();
+        CreateMap<UpdateSliderDto, Slider>().ReverseMap();
+        CreateMap<GetSliderDto, Slider>().ReverseMap();
+        #endregion
+    }
+}
