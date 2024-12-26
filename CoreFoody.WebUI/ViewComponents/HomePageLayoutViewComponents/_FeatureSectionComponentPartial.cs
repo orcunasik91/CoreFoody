e5e1a64033d2cdaafc7a260d9e61f7ec22b3d19c
Orçun Aşık <asikorcun@gmail.com>
2024-12-26 22:50:37 +0300

    Prepared ViewComponent structure for Feature model

---
+++ CoreFoody.WebUI/ViewComponents/HomePageLayoutViewComponents/_FeatureSectionComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using CoreFoody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFoody.WebUI.ViewComponents.HomePageLayoutViewComponents;
+public class _FeatureSectionComponentPartial : ViewComponent
+{
+    private readonly IFeatureService _featureService;
+
+    public _FeatureSectionComponentPartial(IFeatureService featureService)
+    {
+        _featureService = featureService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var features = _featureService.GetAll();
+        return View(features);
+    }
+}
