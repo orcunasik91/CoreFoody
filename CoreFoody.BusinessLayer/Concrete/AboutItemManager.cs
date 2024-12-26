238ff02f9d69085f48824846cec1da181a49da3f
Orçun Aşık <asikorcun@gmail.com>
2024-12-26 21:20:48 +0300

    Added DAL and Business operations for AboutItem model

---
+++ CoreFoody.BusinessLayer/Concrete/AboutItemManager.cs
@@ -0,0 +1,39 @@
+﻿using CoreFoody.BusinessLayer.Abstract;
+using CoreFoody.DataAccessLayer.Abstract;
+using CoreFoody.EntityLayer.Concrete;
+
+namespace CoreFoody.BusinessLayer.Concrete;
+public class AboutItemManager : IAboutItemService
+{
+    private readonly IAboutItemDal _aboutItemDal;
+
+    public AboutItemManager(IAboutItemDal aboutItemDal)
+    {
+        _aboutItemDal = aboutItemDal;
+    }
+
+    public void Delete(int id)
+    {
+        _aboutItemDal.Delete(id);
+    }
+
+    public List<AboutItem> GetAll()
+    {
+        return _aboutItemDal.GetAll();
+    }
+
+    public AboutItem GetById(int id)
+    {
+        return _aboutItemDal.GetById(id);
+    }
+
+    public void Insert(AboutItem entity)
+    {
+        _aboutItemDal.Insert(entity);
+    }
+
+    public void Update(AboutItem entity)
+    {
+        _aboutItemDal.Update(entity);
+    }
+}
---
+++ CoreFoody.DataAccessLayer/EntityFramework/EfAboutItemDal.cs
@@ -0,0 +1,12 @@
+﻿using CoreFoody.DataAccessLayer.Abstract;
+using CoreFoody.DataAccessLayer.Context;
+using CoreFoody.DataAccessLayer.Repositories;
+using CoreFoody.EntityLayer.Concrete;
+
+namespace CoreFoody.DataAccessLayer.EntityFramework;
+public class EfAboutItemDal : BaseRepository<AboutItem>, IAboutItemDal
+{
+    public EfAboutItemDal(FoodyDbContext context) : base(context)
+    {
+    }
+}
