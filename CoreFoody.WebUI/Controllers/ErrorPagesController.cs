073c4bb5fba9324bc0066699199bc0c1cca1483e
Orçun Aşık <asikorcun@gmail.com>
2024-12-26 17:44:56 +0300

    Created Error controller and Page404 page

---
+++ CoreFoody.WebUI/Controllers/ErrorPagesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFoody.WebUI.Controllers;
+public class ErrorPagesController : Controller
+{
+    public IActionResult ErrorPage404()
+    {
+        return View();
+    }
+}
