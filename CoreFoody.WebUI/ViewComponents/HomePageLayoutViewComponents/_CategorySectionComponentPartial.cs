3e480b9d6d5c7acf1c16bf8fd90b15343402ba2d
Orçun Aşık <asikorcun@gmail.com>
2025-02-07 20:05:14 +0300

    Added CategorySeciton component class

---
+++ CoreFoody.WebUI/ViewComponents/HomePageLayoutViewComponents/_CategorySectionComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using CoreFoody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFoody.WebUI.ViewComponents.HomePageLayoutViewComponents;
+public class _CategorySectionComponentPartial : ViewComponent
+{
+    ICategoryService _categoryService;
+
+    public _CategorySectionComponentPartial(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+    public IViewComponentResult Invoke()
+    {
+        var categories = _categoryService.GetAll();
+        return View(categories);
+    }
+}
