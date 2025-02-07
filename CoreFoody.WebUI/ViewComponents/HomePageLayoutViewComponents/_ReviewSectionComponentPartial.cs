e7919284fcfb2e114ef8b8253a418d71ad9cf2ea
Orçun Aşık <asikorcun@gmail.com>
2025-02-07 20:06:10 +0300

    Added ReviewSection component class

---
+++ CoreFoody.WebUI/ViewComponents/HomePageLayoutViewComponents/_ReviewSectionComponentPartial.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CoreFoody.BusinessLayer.Abstract;
+using CoreFoody.DtoLayer.Dtos.ReviewDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFoody.WebUI.ViewComponents.HomePageLayoutViewComponents;
+public class _ReviewSectionComponentPartial : ViewComponent
+{
+    private readonly IReviewService reviewService;
+    private readonly IMapper mapper;
+
+    public _ReviewSectionComponentPartial(IReviewService _reviewService, IMapper _mapper)
+    {
+        reviewService = _reviewService;
+        mapper = _mapper;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var reviews = reviewService.GetAll();
+        List<ResultReviewDto> customerComments = mapper.Map<List<ResultReviewDto>>(reviews);
+        return View(customerComments);
+    }
+}
