b16725f750ce2e30756674246aeaf4f687856668
Orçun Aşık <asikorcun@gmail.com>
2025-02-07 20:05:42 +0300

    Added ProductSection component class

---
+++ CoreFoody.WebUI/ViewComponents/HomePageLayoutViewComponents/_ProductSectionComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using CoreFoody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreFoody.WebUI.ViewComponents.HomePageLayoutViewComponents;
+public class _ProductSectionComponentPartial : ViewComponent
+{
+    private readonly IProductService _productService;
+
+    public _ProductSectionComponentPartial(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var productsLast12Items = _productService.ProductsWithCategoryAndLast12Items();
+        return View(productsLast12Items);
+    }
+}
