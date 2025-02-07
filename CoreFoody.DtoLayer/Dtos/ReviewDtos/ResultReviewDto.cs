97e2c6704db7b7844ce1eb9965dbf673141c6ab0
Orçun Aşık <asikorcun@gmail.com>
2025-02-07 20:09:26 +0300

    Created ResultViewDto

---
+++ CoreFoody.DtoLayer/Dtos/ReviewDtos/ResultReviewDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreFoody.DtoLayer.Dtos.ReviewDtos;
+public class ResultReviewDto
+{
+    public int ReviewId { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerTitle { get; set; }
+    public string Comment { get; set; }
+    public string ImageUrl { get; set; }
+}
